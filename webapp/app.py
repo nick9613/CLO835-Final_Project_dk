5b6e333e9726a04f8fd68d2d14843a6db8179c7f
dkhan0103 <125907962+dkhan0103@users.noreply.github.com>
2023-08-13 23:47:45 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -43,23 +43,7 @@ db_conn = connections.Connection(
 output = {}
 table = 'employee';
 
-# # Define the supported color codes
-# color_codes = {
-#     "red": "#e74c3c",
-#     "green": "#16a085",
-#     "blue": "#89CFF0",
-#     "blue2": "#30336b",
-#     "pink": "#f4c2c2",
-#     "darkblue": "#130f40",
-#     "lime": "#C1FF9C",
-# }
 
-
-# # Create a string of supported colors
-# SUPPORTED_COLORS = ",".join(color_codes.keys())
-
-# # Generate a random color
-# COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
 
 # Define the path where you'll save the downloaded image
@@ -149,27 +133,6 @@ def FetchData():
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
                            lname=output["last_name"], interest=output["primary_skills"], location=output["location"],  background_image=BACKGROUND_IMAGE_PATH, GROUP_NAME=GROUP_NAME)
 
-# if __name__ == '__main__':
-    
-#     # Check for Command Line Parameters for color
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--color', required=False)
-#     args = parser.parse_args()
 
-#     if args.color:
-#         print("Color from command line argument =" + args.color)
-#         COLOR = args.color
-#         if COLOR_FROM_ENV:
-#             print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-#     elif COLOR_FROM_ENV:
-#         print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-#         COLOR = COLOR_FROM_ENV
-#     else:
-#         print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-#     # Check if input color is a supported one
-#     if COLOR not in color_codes:
-#         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-#         exit(1)
 
 app.run(host='0.0.0.0',port=81,debug=True)
